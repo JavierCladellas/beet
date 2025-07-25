5bf58956a04bae4911c566ede4e8fd7e007a8e8a
Javier Cladellas <javiercladellas@gmail.com>
2025-07-25 17:57:23 +0200

    init erp #22

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import { useEffect } from 'react';
-import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-
-import navbar_buttons from './data/NavbarButtons.json';
-import home_sections from './data/HomeSections.json'
-import personaliza_sections from './data/PersonalizaSections.json'
-import corporate_sections from './data/CorporateSections.json'
-import about_sections from './data/AboutSections.json'
-import coordinates from './data/Coordinates.json'
-
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Personaliza from './pages/Personaliza';
-import Corporate from './pages/Corporate';
-import About from './pages/About';
-
-import WhatsappButton from './components/IconButtons';
-
-import './styles/App.css';
-import './styles/text.css'
-
-const ScrollToTop = () => {
-    const { pathname } = useLocation();
-    useEffect(() => { window.scrollTo(0, 0);}, [pathname]);
-    return null;
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-return (
-    <HashRouter>
-        <ScrollToTop />
-        <div className="App">
-
-            <div className='logo-wrapper'>
-                <img className="navbar-logo" src="/beet/logos/beet.png" alt="Logo"/>
-            </div>
-            <Navbar navbar_buttons = {navbar_buttons}/>
-            <Routes>
-                <Route exact path="/" element = { <Home sections={home_sections}/> } />
-                <Route exact path="/personaliza" element = { <Personaliza sections={personaliza_sections}/> } />
-                <Route exact path="/corporate-gifting" element = { <Corporate sections={corporate_sections}/> } />
-                <Route exact path="/about" element={ <About sections={about_sections} />} />
-            </Routes>
-            <Footer />
-        </div>
-        <WhatsappButton number = {coordinates.wa_number} text = {coordinates.wa_default_text}/>
-    </HashRouter>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
