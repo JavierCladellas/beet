44c8b6f1d3560d69deddd9301c4118d2329b6130
Javier Cladellas <javiercladellas@gmail.com>
2025-07-26 15:48:52 +0200

    start validation style

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,18 +1,28 @@
 import "../styles/FormInput.css";
+import React, { useState } from "react";
 
 
 const TextInput = ( props ) => {
-return(
-    props.type === "textarea" ?
-    <div className="input-container">
-        <textarea className="text-input" required={props.required ?? false} id={props.id} name={props.id}></textarea>
-        <label htmlFor={props.id}>{props.label}</label>
-    </div> :
-    <div className="input-container text-input">
-        <input className="text-input" required={props.required ?? false} type={props.type ?? "text"} id={props.id} name={props.id}/>
-        <label htmlFor={props.id}>{props.label}</label>
-    </div>
-)
+    const [isEmpty, setIsEmpty] = useState(true);
+
+    const inputHandler = (e) => {
+        setIsEmpty(e.target.value.trim() === "");
+    }
+    const textAreaHandler = (e) => {
+        inputHandler(e);
+    }
+
+    return(
+        props.type === "textarea" ?
+        <div className="input-container">
+            <textarea className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} id={props.id} name={props.id} onChange={inputHandler}></textarea>
+            <label htmlFor={props.id}>{props.label}</label>
+        </div> :
+        <div className="input-container text-input">
+            <input className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} type={props.type ?? "text"} id={props.id} name={props.id} onChange={textAreaHandler}/>
+            <label htmlFor={props.id}>{props.label}</label>
+        </div>
+    )
 }
 
 export default TextInput;
