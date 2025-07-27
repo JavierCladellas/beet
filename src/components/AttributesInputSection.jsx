737444241ba8d63b2d09443c25bae6ab66694bd2
Javier Cladellas <javiercladellas@gmail.com>
2025-07-27 10:19:55 +0200

    add delete functionality to attribute row

--- src/components/AttributesInputSection.jsx
+++ src/components/AttributesInputSection.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import '../styles/Form.css'
+import Dropdown from './Dropdown';
+import { FaTrash } from "react-icons/fa";
 
-const AttributeInputRow = ( {index} ) => {
+const AttributeInputRow = ( {index, onDelete} ) => {
 return (
-    <div className="form-row">
-        <div className="input-container">
-            <select required id={"fattribute_name_"+index} name={"fattribute_name_"+index} defaultValue="">
-                <option value="" disabled hidden></option>
-                <option value="admin">Admin</option>
-            </select>
-            <label htmlFor={"fattribute_name_"+index}>Atributo</label>
-        </div>
-        <div className="input-container">
-            <select required id="fattribute_value0" name="fattribute_value0" defaultValue="">
-                <option value="" disabled hidden></option>
-                <option value="admin">Admin</option>
-            </select>
-            <label htmlFor="fattribute_value0">Valor</label>
+    <div className="form-row align-center tight">
+        <Dropdown id={"fattribute_name_"+index}
+                    label="Atributo" required
+                    options={[
+                        {"value":"color", "label":"Color"},
+                        {"value":"size", "label":"Talla"},
+                        {"value":"other", "label":"Otro"}
+                    ]}
+        />
+        <Dropdown id={"fattribute_value_"+index}
+                label="Valor" required
+                options={[
+                    {"value":"red", "label":"Rojo"},
+                    {"value":"blue", "label":"Azul"},
+                    {"value":"green", "label":"Verde"},
+                    {"value":"other", "label":"Otro"}
+                ]}
+        />
+        <div className='icon-buttons-container'>
+            <button className='icon-button' type="button" onClick={() => onDelete(index)} >
+                <FaTrash />
+            </button>
         </div>
     </div>
 );
@@ -24,20 +34,38 @@ return (
 
 const AttributeInputSection = ( props ) => {
     const [attributeRows, setAttributeRows] = useState([]);
+    const attributesListRef = useRef(null);
+
     const addAttributeRow = ( ) => {
         setAttributeRows(attributeRows => [...attributeRows, <AttributeInputRow key={attributeRows.length} index={attributeRows.length} />]);
+        setTimeout(() => {
+            if (attributesListRef.current) {
+                attributesListRef.current.scrollTop = attributesListRef.current.scrollHeight;
+            }
+        }, 0);
     }
+
+    const deleteAttributeRow = (indexToDelete) => {
+        setAttributeRows(rows =>
+            rows.filter((_, i) => i !== indexToDelete)
+        );
+    };
+
+
     return (
         <div className='form-col'>
         {attributeRows.length > 0 &&
-        <div className="form-col dynamic-col">
+        <div className="form-col dynamic-col" ref={attributesListRef}>
             {attributeRows.map((row, index) => (
-                <AttributeInputRow key={index} index={index}/>
+                <AttributeInputRow key={index}
+                                index={index}
+                                onDelete={deleteAttributeRow}
+                />
             ))}
         </div>
         }
 
-        <button className='transparent-add-button rotate' type="button" onClick={addAttributeRow}>
+        <button className='transparent-add-button' type="button" onClick={addAttributeRow}>
             + Atributo
         </button>
     </div>
