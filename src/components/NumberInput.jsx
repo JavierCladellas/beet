b2f5a0875e39a56ea44aea3cec5543c6b352b7bd
Javier Cladellas <javiercladellas@gmail.com>
2025-07-27 10:36:49 +0200

    validation number

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,15 +1,23 @@
 import "../styles/FormInput.css";
+import { useState } from "react";
 
 
 const NumberInput = ( props ) => {
+    const [isEmpty, setIsEmpty] = useState(true);
+
+    const inputHandler = (e) => {
+        setIsEmpty(e.target.value.trim() === "");
+    }
 return(
     <div className="number-input-container">
         <label htmlFor={props.id}>{props.label}</label>
-        <input className="number-input" type="number" required={props.required ?? false}
+        <input className={"number-input"+ (isEmpty?"":" not-empty")} type="number" required={props.required ?? false}
             id={props.id} name={props.id}
             defaultValue={props.default_value ?? ""}
             step = {props.step ?? "1"}
             pattern={props.pattern ?? "\\d*"}
+            min={props.min ?? "0"}
+            onChange={inputHandler}
         />
     </div>
 
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,5 +1,5 @@
 import "../styles/FormInput.css";
-import React, { useState } from "react";
+import { useState } from "react";
 
 
 const TextInput = ( props ) => {
@@ -15,11 +15,11 @@ const TextInput = ( props ) => {
     return(
         props.type === "textarea" ?
         <div className="input-container">
-            <textarea className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} id={props.id} name={props.id} onChange={inputHandler}></textarea>
+            <textarea className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} id={props.id} name={props.id} onChange={textAreaHandler}></textarea>
             <label htmlFor={props.id}>{props.label}</label>
         </div> :
         <div className="input-container text-input">
-            <input className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} type={props.type ?? "text"} id={props.id} name={props.id} onChange={textAreaHandler}/>
+            <input className={"text-input"+ (isEmpty?"":" not-empty")} required={props.required ?? false} type={props.type ?? "text"} id={props.id} name={props.id} onChange={inputHandler}/>
             <label htmlFor={props.id}>{props.label}</label>
         </div>
     )
